1ad3df86c8e3b3e520daab0279a2288da00ccd19
Goulart <gustavoggoliveira98@gmail.com>
2022-06-20 10:42:54 -0300

    jogo finalizado com o alerta/sem as animações

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
 /* VARIÁVEIS GLOBAIS */
 
 let numCartas;
-let cartasEscolhidas = [];
+let chosenCard = [];
 i = 0;
+let cardTurned = true;
 /* CONTAINER COM AS CARTAS */
 
 let cards = document.querySelector(".site-container");
 
-/* NUMERO DE JOGADAS E CARTA VIRADA */
+/* NUMERO DE JOGADAS */
 
-let turnCards = true;
 let numJogadas = 0;
 
 /* FUNÇÃO PARA CHAMAR OS CARDS */
 
 function addCards() {
   while (numCartas < 4 || numCartas > 14 || numCartas % 2 !== 0) {
-    numCartas = prompt(
-      `Com quantas cartas você quer jogar?(escolha um número entre 4 e 14 que não seja ímpar)`
+    numCartas = parseInt(
+      prompt(
+        `Com quantas cartas você quer jogar?(escolha um número entre 4 e 14 que não seja ímpar)`
+      )
     );
   }
 
@@ -43,11 +45,12 @@ function addCards() {
 
   for (let i = 0; i < numCartas; i++) {
     cards.innerHTML += `
-    <div class="card" onclick="virarCarta(this)" >
-    <div class="front-face body-card" > 
+    <div class="card" onclick="turnCards(this)
+    " >
+    <div class="front-card body-card" > 
     <img src="${evenCards[i]}" /> 
     </div>
-    <div class="back-face body-card" ><img src="/imgs/front.png">
+    <div class="back-card body-card" ><img src="/imgs/front.png">
     </div>
     </div>
     `;
@@ -61,3 +64,56 @@ function twistCards() {
 }
 
 addCards();
+
+/* FUNÇÃO PARA VIRAR AS CARTAS */
+
+function turnCards(element) {
+  if (cardTurned) {
+    if (chosenCard.length <= 2) {
+      numJogadas++;
+      i++;
+      element.classList.add("chosen");
+      chosenCard.push(element);
+      console.log(chosenCard.length);
+
+      if (i === 2) {
+        if (chosenCard[i - 2].innerHTML === chosenCard[i - 1].innerHTML) {
+          chosenCard[i - 2].removeAttribute("onclick");
+          chosenCard[i - 1].removeAttribute("onclick");
+          i = 0;
+          chosenCard = [];
+          setTimeout(endGame, 1000);
+        } else {
+          cardTurned = false;
+          setTimeout(unturnCard, 1000);
+        }
+      }
+    }
+  }
+}
+
+/* FUNÇÃO DE DESVIRAR AS CARTAS */
+
+function unturnCard() {
+  chosenCard[i - 2].classList.remove("chosen");
+  chosenCard[i - 1].classList.remove("chosen");
+  chosenCard = [];
+  i = 0;
+  cardTurned = true;
+}
+
+/* FIM DO JOGO */
+
+function endGame() {
+  turned = document.querySelectorAll(".chosen");
+
+  if (turned.length === numCartas) {
+    alert("Parabéns, você ganhou em " + numJogadas + " jogadas");
+    let restart = prompt(
+      "Gostaria de jogar novamente?(Digite 'sim' para jogar novamente)"
+    );
+    if (restart === "sim") {
+      window.location.reload(true);
+    }
+  }
+}
